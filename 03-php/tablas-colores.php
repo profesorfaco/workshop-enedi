c16121eb90201a10d896f4a6fa7a6fbfc8f7da1e
FACO <hola@faco.cl>
2017-10-18 18:47:52 -0300

    Update tablas-colores.php

--- 03-php/tablas-colores.php
+++ 03-php/tablas-colores.php
@@ -21,16 +21,33 @@
                 <th>Carrera</th>
                 <th>Institución</th>
                 <th>Acreditada</th>
+                <th>Emoticon</th>
             </tr>
+
             <?php $noacreditadas=0; $acreditadas=0;?>
             <?php for($n = 1; $n < count($dato); $n++){?>
                 <?php if(substr_count($dato[$n]['institution'],'UNIVERSIDAD') > 0){?>
 
-                <tr>
-                    <td><?php print($dato[$n]['career']);?></td>
-                    <td><?php print($dato[$n]['institution']);?></td>
-                    <td class="text-center"><img src="images/<?php print($dato[$n]['years']);?>.png"></td>
-                </tr>
+<tr>
+<td><?php print($dato[$n]['career']);?></td>
+<td><?php print($dato[$n]['institution']);?></td>
+<td class="text-center">
+    <img src="images/<?php print($dato[$n]['years']);?>.png">
+</td>
+<td class="text-center">
+<?php if(($dato[$n]['years'])==7){?>
+        :-D
+    <?php }else if(($dato[$n]['years'])==6||($dato[$n]['years'])==5){ ?>
+        :-)
+    <?php }else if(($dato[$n]['years'])==4||($dato[$n]['years'])==3){ ?>
+        :-/
+    <?php }else if(($dato[$n]['years'])==2||($dato[$n]['years'])==1){ ?>
+        :-(
+    <?php }else{ ?>
+        X-(
+    <?php }; ?>
+</td>
+</tr>
 
                 <?php if(($dato[$n]['result'])=="ACREDITADA"){?>
                     <?php $acreditadas++;?>
